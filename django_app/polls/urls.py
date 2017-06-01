45fcf6dd2c533eecac39a5688736f850c25d3aa6
Yunsik Park <space214@gmail.com>
2017-06-02 06:40:21 +0900

    write simple form for vote and result pages / Less code is better | Amend URLconf and Amend views

--- django_app/polls/urls.py
+++ django_app/polls/urls.py
@@ -5,11 +5,14 @@ from polls import views
 app_name = 'polls'
 urlpatterns = [
     # ex: /polls/
-    url(r'^$', views.index, name='index'),
+    # url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
     # ex: /polls/5/
-    url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
+    # url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     # ex: /polls/5/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.result, name='results'),
+    # url(r'^(?P<question_id>\d+)/results/$', views.result, name='results'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     # ex: /polls/5/vote/
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
